41841f47eb11f3a97cc773514787ac096d6e8c9a
All Eyes <matthewacha@gmail.com>
2017-08-16 06:48:32 +0300

    python/module/test_users.py

--- python/modules/test_users.py
+++ python/modules/test_users.py
@@ -1,1 +1,16 @@
 """test_users"""
+import unittest
+import users
+from users import input_password,test_input_email,test_input_number
+
+class test_users(unittest.Testcase):
+    def test_input_password(self):
+        self.assertEqual(input_password('man'),'man')
+
+    def test_input_email(self):
+        self.assertEqual(input_email('ap@gmail.com'),'ap@gmail.com')
+
+    def test_input_number(self):
+        self.assertEqual(input_number('12345'),'12345')
+
+            
