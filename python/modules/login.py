ecbd48e674a8a61a657eab6fe502dd142dff960f
All Eyes <matthewacha@gmail.com>
2017-08-17 07:48:47 +0300

    fixed login feature

--- python/modules/login.py
+++ python/modules/login.py
@@ -1,6 +1,6 @@
 """login feature,check if a value entered exists in the data based"""
 import data_storage
-from data_storage import user_data,assign_details
+from data_storage import user_data
 
 class login(object):
     def __init__(self,email,password):
@@ -10,8 +10,6 @@ class login(object):
 
       
     def check_email_password(self,email,password):
-        """email_address=raw_input('')
-        password=raw_input('')"""
         if self.email in data_storage.user_data.details.items():
             n=details[self.email]
             if password==n:
