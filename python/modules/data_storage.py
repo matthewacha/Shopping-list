cc97864b20e9072f11bd903cc253f3412fb1df98
All Eyes <matthewacha@gmail.com>
2017-08-17 08:51:36 +0300

    remove email

--- python/modules/data_storage.py
+++ python/modules/data_storage.py
@@ -13,12 +13,11 @@ class user_data(object):
           if self.email!=self.details.keys() and self.password!=self.details[self.email]:   
             self.details[self.email]=self.password
         return self.details
-    
-    """def change_password_email(self,email,password,details):
+    def remove_password_email(self,email,password,details):
         for self.email,self.password in self.details.items():    
           if self.email!=self.details.keys() and self.password!=self.details[self.email]:   
-            self.details[self.email]=self.password
-        return self.details"""
+            self.details.pop(self.email)
+        return self.details
 
     def add_item(self,item_name,quantity): 
         try:     
