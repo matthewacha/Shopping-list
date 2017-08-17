5d2735a3c4d2d44c076bbc57aa9800facc9907ed
All Eyes <matthewacha@gmail.com>
2017-08-17 10:21:59 +0300

    updated user details

--- python/modules/data_storage.py
+++ python/modules/data_storage.py
@@ -1,31 +1,32 @@
 """stores information of users"""
 class user_data(object):
-    def __init__(self,password,email,number,item_name,items={},quantity=0,details={}):
+    def __init__(self,password,email,number,item_name,items={},quantity=0,user_details={}):
         self.password=password
         self.email=email
         self.number=number
-        self.details=details
+        self.user_details=user_details
         self.quantity=quantity
         self.items=items
         self.item_name=item_name
-    def add_password_email(self,email,password,details):
-        for self.email,self.password in self.details.items():    
-          if self.email!=self.details.keys() and self.password!=self.details[self.email]:   
-            self.details[self.email]=self.password
-        return self.details
-    def remove_password_email(self,email,password,details):
-        for self.email,self.password in self.details.items():    
-          if self.email!=self.details.keys() and self.password!=self.details[self.email]:   
-            self.details.pop(self.email)
-        return self.details
+    def add_password_email(self,email,password,user_details):
+        for self.email,self.password in self.user_details.items():    
+          if self.email!=self.user_details.keys() and self.password!=self.user_details[self.email]:   
+            self.user_details[self.email]=self.password
+        return self.user_details
 
-    def add_item(self,item_name,quantity): 
+    def remove_password_email(self,email,password,user_details):
+        for self.email,self.password in self.user_details.items():    
+          if self.email!=self.user_details.keys() and self.password!=self.user_details[self.email]:   
+            self.user_details.pop(self.email)
+        return self.user_details
+    """update items in lists"""
+    def add_item(self,item_name,quantity,items): 
         try:     
           self.items[self.item_name] += self.quantity
         except(KeyError,AttributeError):
             pass
         return  self.items
 
-    def remove_item(self,quantity,item_name):
+    def remove_item(self,item_name,quantity,items):
         self.quantity-=self.items[self.item_name]
         return self.items
--- python/modules/view.py
+++ python/modules/view.py
@@ -1,4 +1,6 @@
 """list data storage and manipulation"""
+import data_storage 
+from data_storage import user_data
 
 class shopping_lists(object):
     """This adds and deletes lists of a user"""
@@ -9,24 +11,14 @@ class shopping_lists(object):
       self.item_group=item_group
 
     def add_list(self,shoplist,list_group):
-      
-
-      return key
+      self.list_group[self.shoplist]=data_storage.user_data.user_details
+      return self.list_group.key(self.shoplist)
 
     def delete_list(self,shoplist,list_group):
-      list_group.remove(shoplist)
+      self.list_group.pop(self.shoplist)
+      return self.list_group
 
-     def check_list(self,shoplist,list_group): 
-    """this adds and deletes items from a specific list"""
-class list_items(shopping_lists):
-    def __init__(self,items={},quantity=0):
-        self.items=items
-        self.quantity=quantity
+     def check_list(self,shoplist,list_group):
+         return self.list_group.key(self.shoplist)
 
-    def add_item(self,item_name,quantity):      
-        self.items[self.item_name]+=self.quantity
-        return  self.items
-
-    def remove_item(self,quantity,item_name):
-        self.quantity-=self.items[self.item_name]
-        return self.items   
+  
